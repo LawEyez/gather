38e816c4f1493ece97039413e63bf2a27f121faf
LawEyez <allenojuka10@gmail.com>
2024-02-01 05:05:49 +0300

    Fix ts issue.

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -102,7 +102,7 @@ export async function POST(req: Request) {
   if (eventType === 'user.deleted') {
     const { id } = evt.data
 
-    const deletedUser = await deleteUser(id)
+    const deletedUser = await deleteUser(id as string)
 
     return NextResponse.json({
       message: 'OK',
