d56100a9a215ce1651aadf8d83f8d95d35a7050a
LawEyez <allenojuka10@gmail.com>
2024-02-01 05:54:14 +0300

    Move to clerk folder.

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -47,7 +47,7 @@ export async function POST(req: Request) {
   }
 
   // Get the ID and type
-  const { id } = evt.data
+  // const { id } = evt.data
   const eventType = evt.type
 
   // On user create.
